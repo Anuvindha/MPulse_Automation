ae15bb7661baaee058394d4b54be9b6b522ad19d
Anuvindha <68215932+Anuvindha@users.noreply.github.com>
2020-07-23 11:13:44 +0530

    Add files via upload

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,435 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import java.lang.String
+
+import com.kms.katalon.core.testobject.TestObject
+
+
+def static "purchaseReqRecord.backorder_POR.backordercreate"() {
+    (new purchaseReqRecord.backorder_POR()).backordercreate()
+}
+
+def static "search.search_MA.searchMA"(
+    	String RecordName	) {
+    (new search.search_MA()).searchMA(
+        	RecordName)
+}
+
+def static "search.INV_search_creation_013.INV_search_create"() {
+    (new search.INV_search_creation_013()).INV_search_create()
+}
+
+def static "maintenance_Records.MR_Creation_FormLayout.MRCreation"(
+    	String woDescription	
+     , 	String assetName	
+     , 	String inventoryName	) {
+    (new maintenance_Records.MR_Creation_FormLayout()).MRCreation(
+        	woDescription
+         , 	assetName
+         , 	inventoryName)
+}
+
+def static "scheduled_Maintenance_Records.SMR_Creation.SMRCreationFixed"(
+    	String SMRDescription	
+     , 	String taskName	
+     , 	String assetName	
+     , 	String inventoryName	) {
+    (new scheduled_Maintenance_Records.SMR_Creation()).SMRCreationFixed(
+        	SMRDescription
+         , 	taskName
+         , 	assetName
+         , 	inventoryName)
+}
+
+def static "scheduled_Maintenance_Records.SMR_Creation.SMRCreationFloating"(
+    	String SMRDescription	
+     , 	String taskName	
+     , 	String assetName	
+     , 	String inventoryName	) {
+    (new scheduled_Maintenance_Records.SMR_Creation()).SMRCreationFloating(
+        	SMRDescription
+         , 	taskName
+         , 	assetName
+         , 	inventoryName)
+}
+
+def static "scheduled_Maintenance_Records.SMR_Creation.SMRCreationMeterBased"(
+    	String SMRDescription	
+     , 	String taskName	
+     , 	String assetName	
+     , 	String inventoryName	) {
+    (new scheduled_Maintenance_Records.SMR_Creation()).SMRCreationMeterBased(
+        	SMRDescription
+         , 	taskName
+         , 	assetName
+         , 	inventoryName)
+}
+
+def static "inventory_clerk.INV_Clerk_Redirect.INV_redirect"() {
+    (new inventory_clerk.INV_Clerk_Redirect()).INV_redirect()
+}
+
+def static "search.Search_Eqp_till_MA.eqpselect"() {
+    (new search.Search_Eqp_till_MA()).eqpselect()
+}
+
+def static "search.WKO_Open_Close_004.close_wko"() {
+    (new search.WKO_Open_Close_004()).close_wko()
+}
+
+def static "work_Order.WO_Hold_FormLayout.holdWorkOrderFormLayout"() {
+    (new work_Order.WO_Hold_FormLayout()).holdWorkOrderFormLayout()
+}
+
+def static "search.pending_MRR_to_WKO.pendingtowko"() {
+    (new search.pending_MRR_to_WKO()).pendingtowko()
+}
+
+def static "search.MA_General.searchMA"(
+    	String RecordName	) {
+    (new search.MA_General()).searchMA(
+        	RecordName)
+}
+
+def static "search.MA_General.searchMAForMAtoWO"(
+    	String RecordName	) {
+    (new search.MA_General()).searchMAForMAtoWO(
+        	RecordName)
+}
+
+def static "search.MA_General.searchMAForMRtoWO_New"(
+    	String RecordName	) {
+    (new search.MA_General()).searchMAForMRtoWO_New(
+        	RecordName)
+}
+
+def static "search.INV_Record_creation_012.record_create"() {
+    (new search.INV_Record_creation_012()).record_create()
+}
+
+def static "commonUtils.layout_select.layout_selection"() {
+    (new commonUtils.layout_select()).layout_selection()
+}
+
+def static "maintenance_Records.MR_ConverttoWO.MRtoWO"(
+    	String woDescription	) {
+    (new maintenance_Records.MR_ConverttoWO()).MRtoWO(
+        	woDescription)
+}
+
+def static "work_Order.WO_Close_ListLayout.closeWorkOrderListLayout"() {
+    (new work_Order.WO_Close_ListLayout()).closeWorkOrderListLayout()
+}
+
+def static "media.uploadMediatoWO.uploadMedia"(
+    	String mediaName	) {
+    (new media.uploadMediatoWO()).uploadMedia(
+        	mediaName)
+}
+
+def static "search.search_POR_redirection.redirect_POR"() {
+    (new search.search_POR_redirection()).redirect_POR()
+}
+
+def static "inventory_clerk.inventory_clerk_key.inventsearch"() {
+    (new inventory_clerk.inventory_clerk_key()).inventsearch()
+}
+
+def static "purchaseReqRecord.por_Listview_create.create"() {
+    (new purchaseReqRecord.por_Listview_create()).create()
+}
+
+def static "search.search_EQP_redirection.redirect_EQP"() {
+    (new search.search_EQP_redirection()).redirect_EQP()
+}
+
+def static "search.List_View_Search.listViewSearch_Row"() {
+    (new search.List_View_Search()).listViewSearch_Row()
+}
+
+def static "search.List_View_Search.listViewSearch_Column"() {
+    (new search.List_View_Search()).listViewSearch_Column()
+}
+
+def static "work_Order.WO_Close_FormLayout.closeWorkOrderFormLayout"() {
+    (new work_Order.WO_Close_FormLayout()).closeWorkOrderFormLayout()
+}
+
+def static "purchaseReqRecord.create_POR.create"() {
+    (new purchaseReqRecord.create_POR()).create()
+}
+
+def static "work_Order.WO_EditAndSave_ListLayout.editAndSave"(
+    	String woID	) {
+    (new work_Order.WO_EditAndSave_ListLayout()).editAndSave(
+        	woID)
+}
+
+def static "work_Order.WO_FormLayout_Redirection.workOrderFormLayoutRedirection"() {
+    (new work_Order.WO_FormLayout_Redirection()).workOrderFormLayoutRedirection()
+}
+
+def static "commonUtils.Common.navigateToURL"(
+    	String url	) {
+    (new commonUtils.Common()).navigateToURL(
+        	url)
+}
+
+def static "commonUtils.Common.redirectToMA"() {
+    (new commonUtils.Common()).redirectToMA()
+}
+
+def static "commonUtils.Common.login"(
+    	String userName	
+     , 	String password	
+     , 	String databaseName	) {
+    (new commonUtils.Common()).login(
+        	userName
+         , 	password
+         , 	databaseName)
+}
+
+def static "commonUtils.Common.woInfoLinkSearch"(
+    	String object	
+     , 	String value	
+     , 	String xpathvalue	
+     , 	String object1	) {
+    (new commonUtils.Common()).woInfoLinkSearch(
+        	object
+         , 	value
+         , 	xpathvalue
+         , 	object1)
+}
+
+def static "commonUtils.Common.selectValueFromDrop"(
+    	String object	
+     , 	String value	
+     , 	String value2	
+     , 	String object1	) {
+    (new commonUtils.Common()).selectValueFromDrop(
+        	object
+         , 	value
+         , 	value2
+         , 	object1)
+}
+
+def static "commonUtils.Common.selectValueFromDrop1"(
+    	String object	
+     , 	String value	
+     , 	String value2	
+     , 	String object1	
+     , 	String value3	) {
+    (new commonUtils.Common()).selectValueFromDrop1(
+        	object
+         , 	value
+         , 	value2
+         , 	object1
+         , 	value3)
+}
+
+def static "commonUtils.Common.editandsave"(
+    	String object	
+     , 	String value	
+     , 	String value2	) {
+    (new commonUtils.Common()).editandsave(
+        	object
+         , 	value
+         , 	value2)
+}
+
+def static "commonUtils.Common.enterValueInTextField"(
+    	String object	
+     , 	String value	) {
+    (new commonUtils.Common()).enterValueInTextField(
+        	object
+         , 	value)
+}
+
+def static "commonUtils.Common.enterValueInTextField1"(
+    	String object	
+     , 	String value	
+     , 	String text	) {
+    (new commonUtils.Common()).enterValueInTextField1(
+        	object
+         , 	value
+         , 	text)
+}
+
+def static "commonUtils.Common.tabswitch"(
+    	String object	
+     , 	String value	) {
+    (new commonUtils.Common()).tabswitch(
+        	object
+         , 	value)
+}
+
+def static "commonUtils.Common.linkandreplace"(
+    	String object	
+     , 	String value	) {
+    (new commonUtils.Common()).linkandreplace(
+        	object
+         , 	value)
+}
+
+def static "commonUtils.Common.req_status"(
+    	String object	
+     , 	String value	) {
+    (new commonUtils.Common()).req_status(
+        	object
+         , 	value)
+}
+
+def static "commonUtils.Common.calendarDateSelect1"(
+    	String var	) {
+    (new commonUtils.Common()).calendarDateSelect1(
+        	var)
+}
+
+def static "commonUtils.Common.calendarDateSelect"() {
+    (new commonUtils.Common()).calendarDateSelect()
+}
+
+def static "commonUtils.Common.uploadFile"(
+    	TestObject to	
+     , 	String filePath	) {
+    (new commonUtils.Common()).uploadFile(
+        	to
+         , 	filePath)
+}
+
+def static "commonUtils.Common.layoutSelectionList"(
+    	String layoutName	) {
+    (new commonUtils.Common()).layoutSelectionList(
+        	layoutName)
+}
+
+def static "commonUtils.Common.WaitForElementPresent"(
+    	String locator	
+     , 	String locatedBy	) {
+    (new commonUtils.Common()).WaitForElementPresent(
+        	locator
+         , 	locatedBy)
+}
+
+def static "commonUtils.Common.logoutAndCloseBrowser"() {
+    (new commonUtils.Common()).logoutAndCloseBrowser()
+}
+
+def static "commonUtils.Common.getTotalCountOfElement"(
+    	String xPath	) {
+    (new commonUtils.Common()).getTotalCountOfElement(
+        	xPath)
+}
+
+def static "work_Order.WO_Creation_ListLayout.workOrderCreationList"(
+    	String WO_name	) {
+    (new work_Order.WO_Creation_ListLayout()).workOrderCreationList(
+        	WO_name)
+}
+
+def static "search.redirection_INV_click.redirect_INV"() {
+    (new search.redirection_INV_click()).redirect_INV()
+}
+
+def static "work_Order.WO_Creation_FormLayout.workOrderCreation"(
+    	String woDescription	
+     , 	String taskName	
+     , 	String assetName	
+     , 	String inventoryName	) {
+    (new work_Order.WO_Creation_FormLayout()).workOrderCreation(
+        	woDescription
+         , 	taskName
+         , 	assetName
+         , 	inventoryName)
+}
+
+def static "sorting.list_Sorting.listViewSort_Desc"() {
+    (new sorting.list_Sorting()).listViewSort_Desc()
+}
+
+def static "inventory_clerk.INV_search_022.search"(
+    	Object RecordName	) {
+    (new inventory_clerk.INV_search_022()).search(
+        	RecordName)
+}
+
+def static "scheduled_Maintenance_Records.SMR_Redirection.SMRRedirection"() {
+    (new scheduled_Maintenance_Records.SMR_Redirection()).SMRRedirection()
+}
+
+def static "maintenance_Records.MR_ConverttoWO_ListLayout.convertWKOFromListLayout"(
+    	String woDescription	) {
+    (new maintenance_Records.MR_ConverttoWO_ListLayout()).convertWKOFromListLayout(
+        	woDescription)
+}
+
+def static "reports.Form_View.formView_CurrentRecord_New"(
+    	String woDescription	) {
+    (new reports.Form_View()).formView_CurrentRecord_New(
+        	woDescription)
+}
+
+def static "reports.Form_View.formView_CurrentRecord_Old"() {
+    (new reports.Form_View()).formView_CurrentRecord_Old()
+}
+
+def static "reports.Form_View.formView_AllRecord"() {
+    (new reports.Form_View()).formView_AllRecord()
+}
+
+def static "reports.Form_View.formView_CurrentLookup"() {
+    (new reports.Form_View()).formView_CurrentLookup()
+}
+
+def static "purchaseReqRecord.closepur_POR.close"() {
+    (new purchaseReqRecord.closepur_POR()).close()
+}
+
+def static "work_Order.WO_ListView_Redirection.workOrderListLayoutRedirection"() {
+    (new work_Order.WO_ListView_Redirection()).workOrderListLayoutRedirection()
+}
+
+def static "search.search_MRR_redirection.redirect_MRR"() {
+    (new search.search_MRR_redirection()).redirect_MRR()
+}
+
+def static "search.WKO_redirect_create_004.redirect_close"() {
+    (new search.WKO_redirect_create_004()).redirect_close()
+}
+
+def static "reorder_list.reorder_list_key.reorder"() {
+    (new reorder_list.reorder_list_key()).reorder()
+}
+
+def static "search.POR_sample_add_POR.add_POR"() {
+    (new search.POR_sample_add_POR()).add_POR()
+}
+
+def static "reports.List_View.ListView"() {
+    (new reports.List_View()).ListView()
+}
+
+def static "purchaseReqRecord.por_Listview_createandclose.createandclose"() {
+    (new purchaseReqRecord.por_Listview_createandclose()).createandclose()
+}
+
+def static "work_Order.WO_Cancel_FromLayout.cancelWorkOrderFormLayout"() {
+    (new work_Order.WO_Cancel_FromLayout()).cancelWorkOrderFormLayout()
+}
+
+def static "work_Order.WO_Redirection.workOrderRedirection"() {
+    (new work_Order.WO_Redirection()).workOrderRedirection()
+}
+
+def static "maintenance_Records.MR_Creation_ListLayout.MR_Creation_List"(
+    	String woDescription	) {
+    (new maintenance_Records.MR_Creation_ListLayout()).MR_Creation_List(
+        	woDescription)
+}
+
+def static "maintenance_Records.MR_Redirection.maintenanceRecordRedirection"() {
+    (new maintenance_Records.MR_Redirection()).maintenanceRecordRedirection()
+}
